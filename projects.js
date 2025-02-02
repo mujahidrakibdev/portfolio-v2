4976d8cf76b50c309e1bdbecca9b5e216cacd0de
Mujahid <mujahidrakibdev@gmail.com>
2025-02-03 00:20:19 +0600

    added calculator app and new photo of the portfolio

--- projects.js
+++ projects.js
@@ -2,40 +2,53 @@
 export const projects = [
     {
         "id": 1,
-        "image": "images/portfolio.jpg",
+        "image": "images/portfolio.png",
         "title": "Portfolio Website",
         "description": "A responsive and modern portfolio website showcasing a creative professional's work and skills."
     },
     {
         "id": 2,
+        "image": "images/todo-app.png",
+        "title": "ToDo App",
+        "description": "A Simple ToDo app with the functions of adding todos, removing them and mark as completed. the data is stored in the local server"
+    },
+    {
+        "id": 3,
+        "image": "images/calculator-app.png",
+        "title": "Calculator App",
+        "description": "A fully functional Calculator App with function to calculate anything by clicking the on screen buttons and also by computer keyboard buttons."
+    },
+    {
+        "id": 4,
         "image": "images/e-commerce.jpg",
         "title": "E-Commerce Website",
         "description": "A fully functional e-commerce platform with shopping cart, product filtering, and payment gateway integration."
     },
     {
-        "id": 3,
+        "id": 5,
         "image": "images/real-estate.jpg",
         "title": "Real Estate Website",
         "description": "A real estate website with property listings, search functionality, and an interactive map for finding nearby properties."
     },
     {
-        "id": 4,
+        "id": 6,
         "image": "images/restaurant.webp",
         "title": "Restaurant Landing Page",
         "description": "A clean and visually appealing landing page for a restaurant, featuring a menu, online reservations, and location information."
     },
     {
-        "id": 5,
+        "id": 7,
         "image": "images/saas.png",
         "title": "SaaS Product Website",
         "description": "A marketing website for a SaaS product, including features overview, customer testimonials, and a pricing section."
     },
     {
-        "id": 6,
+        "id": 8,
         "image": "images/education.jpg",
         "title": "Educational Blog",
         "description": "A responsive blog website for educational content with user-friendly navigation, categorized posts, and a commenting system."
     }
+    
 ]
 
 
